d3cef4ace4f53183834a0c160108b7982b70d8e7
Spark <79936503+spa5k@users.noreply.github.com>
2022-02-05 19:14:16 +0530

    did few enums

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,11 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
-
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Echo(String),
+    Move(Point),
+    Quit,
+    ChangeColor(u8, u8, u8),
 }
 
 struct Point {
@@ -37,6 +38,13 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+
+        match message {
+            Message::Echo(m) => self.echo(m),
+            Message::Move(m) => self.move_position(m),
+            Message::Quit => self.quit(),
+            Message::ChangeColor(r, g, b) => self.change_color((r, g, b)),
+        }
     }
 }
 
@@ -51,7 +59,7 @@ mod tests {
             position: Point { x: 0, y: 0 },
             color: (0, 0, 0),
         };
-        state.process(Message::ChangeColor((255, 0, 255)));
+        state.process(Message::ChangeColor(255, 0, 255));
         state.process(Message::Echo(String::from("hello world")));
         state.process(Message::Move(Point { x: 10, y: 15 }));
         state.process(Message::Quit);
